5253c70c1f2e042afe015d68d45be7275b35140b
Dionisis <dio@skarp.dk>
2022-05-07 16:54:07 +0300

    Updates from may request

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby';
-import Link from 'gatsby-plugin-transition-link';
 import Helmet from 'react-helmet';
 import Page from '../components/Page';
-import Timeline from '../components/Timeline';
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage';
 import PageLetter from '../components/PageLetter';
 import Beta from '../components/lettersSvg/Beta';
 import { pattern2, pattern3 } from '../constants/patterns';
 import Icon from '../components/Icon';
-import { iconPublications, iconCV } from '../constants/svg'
+import { iconCV } from '../constants/svg'
 
-export const AboutPageTemplate = ({ title, body, image, timeline, helmet, publications, cv,  locale }) => {
+export const AboutPageTemplate = ({ title, body, image, helmet, publications, cv,  locale }) => {
   return (
     <Page locale={locale}>
       {helmet}
@@ -65,31 +63,36 @@ export const AboutPageTemplate = ({ title, body, image, timeline, helmet, public
                   </a>
                 )}
 
-                {publications && (
+                {/*publications && (
                   <Link className="h3" to={`/tags/${publications}`}>
                     <Icon {...iconPublications} />
                     <div className="heading heading--gutters heading--center">
                       {locale === 'en' ? 'Publications' :  'Δημοσιεύσεις'}
                     </div>
                   </Link>
-                )}
+                )*/}
 
               </div>
             )}
 
-            <h2 className="h1 heading heading--gutters heading--center">
-              {timeline.title}
-            </h2>
+            {/*
+              <h2 className="h1 heading heading--gutters heading--center">
+                {timeline.title}
+              </h2>
 
-            <Timeline events={timeline.events} />
+              <Timeline events={timeline.events} />
+            */}
+
           </div>
 
-          <div className="visually-hidden">
-            <svg id="link-icon" className="stroked-icon" viewBox="0 0 85 148" xmlns="http://www.w3.org/2000/svg">
-              <path d="M44.875,1 L40.125,1 C35.375,17 33,29 33,37 C33,45 35.375,57 40.125,73 L44.875,73 C49.625,61 52,49 52,37 C52,25 49.625,13 44.875,1 Z" />
-              <path d="M44.875,75 L40.125,75 C35.375,91 33,103 33,111 C33,119 35.375,131 40.125,147 L44.875,147 C49.625,135 52,123 52,111 C52,99 49.625,87 44.875,75 Z" />
-            </svg>
-          </div>
+          {/*
+            <div className="visually-hidden">
+              <svg id="link-icon" className="stroked-icon" viewBox="0 0 85 148" xmlns="http://www.w3.org/2000/svg">
+                <path d="M44.875,1 L40.125,1 C35.375,17 33,29 33,37 C33,45 35.375,57 40.125,73 L44.875,73 C49.625,61 52,49 52,37 C52,25 49.625,13 44.875,1 Z" />
+                <path d="M44.875,75 L40.125,75 C35.375,91 33,103 33,111 C33,119 35.375,131 40.125,147 L44.875,147 C49.625,135 52,123 52,111 C52,99 49.625,87 44.875,75 Z" />
+              </svg>
+            </div>
+            */}
         </div>
       </div>
     </Page>
